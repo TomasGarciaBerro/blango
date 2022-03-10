deb87bbd847ecee31059efd3c9d02b3bc3e4df95
TomasGarciaBerro <52388933+TomasGarciaBerro@users.noreply.github.com>
2022-03-10 15:40:15 -0300

    Update views.py
    Minor change in query_set() method

--- blog/api/views.py
+++ blog/api/views.py
@@ -69,9 +69,18 @@ class PostViewSet(viewsets.ModelViewSet):
         return Response(serializer.data)
 
     def get_queryset(self):
-        # queryset has been set by applying user filtering rules
+        if self.request.user.is_anonymous:
+            # published only
+            queryset = self.queryset.filter(published_at__lte=timezone.now())
 
-        # fetch the period_name URL parameter from self.kwargs
+        elif not self.request.user.is_staff:
+            # allow all
+            queryset = self.queryset
+        else:
+            queryset = self.queryset.filter(
+                Q(published_at__lte=timezone.now()) | Q(author=self.request.user)
+            )
+
         time_period_name = self.kwargs.get("period_name")
 
         if not time_period_name:
@@ -79,7 +88,9 @@ class PostViewSet(viewsets.ModelViewSet):
             return queryset
 
         if time_period_name == "new":
-            return queryset.filter(published_at__gte=timezone.now() - timedelta(hours=1))
+            return queryset.filter(
+                published_at__gte=timezone.now() - timedelta(hours=1)
+            )
         elif time_period_name == "today":
             return queryset.filter(
                 published_at__date=timezone.now().date(),
