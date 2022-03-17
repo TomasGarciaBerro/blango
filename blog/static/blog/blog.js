ff7c9517fc15a81b782389fd5fee421cdd8e84ac
Tomas Berro <tomasgarciaberro@gmail.com>
2022-03-17 15:19:58 +0000

    Finish JavaScript functions

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,9 +1,18 @@
-const theNumber = 1
-let name = 'Ben'
-
-if (theNumber === 1) {
-  let name = 'Leo'
-  alert(name)
+function sayHello(name) {
+  if (name === undefined) {
+      console.log('Hello, no name')
+  } else {
+       console.log('Hello, ' + name)
+  }
 }
 
-alert(name)
+const name = 'Your Name'  // Put your name here
+
+console.log('Before setTimeout')
+
+setTimeout(() => {
+    sayHello(name)
+  }, 2000
+)
+
+console.log('After setTimeout')
